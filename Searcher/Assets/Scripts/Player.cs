1837c97d9339d76fc49962d9a77418b7b8e8a130
searcherdev <searcherdev@gmail.com>
2026-01-16 16:56:09 -0500

    Assets uploaded & player rotation built

--- Searcher/Assets/Scripts/Player.cs
+++ Searcher/Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -11,8 +12,9 @@ public class Player : MonoBehaviour
 
     //Values
     private float accel; //Will be taken from N0M-AD's Engine
-    private const float decel = -0.01f; //Constant deceleration in space
+    private const float decel = 0.05f; //Constant deceleration in space
     private float maxSpeed;
+    private float rotSpeed;
 
     //Camera
     private Camera cam;
@@ -29,6 +31,7 @@ public class Player : MonoBehaviour
     public float Acceleration { get { return accel; } }
     public float Decel { get { return decel; } }
     public float MaxSpeed { get { return maxSpeed; } }
+    public float RotSpeed { get { return rotSpeed; } }
     
     //==== START ====
     void Start()
@@ -42,18 +45,19 @@ public class Player : MonoBehaviour
         position = transform.position;
 
         //Set movement values (NOTE: WILL BE REPLACED WITH EVENTUAL N0M-AD CLASS VALUES)
-        accel = 0.05f;
-        maxSpeed = 0.5f;
+        accel = 1f;
+        maxSpeed = 5f;
+        rotSpeed = 50f;
     }
 
     //==== UPDATE ====
     void Update()
     {
         //Arrow Key Movement
-        if (Keyboard.current.leftArrowKey.isPressed) velocity.x -= (accel * Time.deltaTime); //Left
-        if (Keyboard.current.rightArrowKey.isPressed) velocity.x += (accel * Time.deltaTime); //Right
-        if (Keyboard.current.downArrowKey.isPressed) velocity.y -= (accel * Time.deltaTime); //Down
-        if (Keyboard.current.upArrowKey.isPressed) velocity.y += (accel * Time.deltaTime); //Up
+        if (Keyboard.current.leftArrowKey.isPressed) { velocity.x -= (accel * Time.deltaTime); } //Left
+        if (Keyboard.current.rightArrowKey.isPressed) { velocity.x += (accel * Time.deltaTime); } //Right
+        if (Keyboard.current.downArrowKey.isPressed) { velocity.y -= (accel * Time.deltaTime); } //Down
+        if (Keyboard.current.upArrowKey.isPressed) { velocity.y += (accel * Time.deltaTime); }//Up
 
         //Clamp Movement Speed
         if (velocity.x <= -maxSpeed) velocity.x = -maxSpeed;
@@ -62,15 +66,27 @@ public class Player : MonoBehaviour
         if (velocity.y >= maxSpeed) velocity.y = maxSpeed;
 
         //Apply Universal Decel
-        if (velocity.x < -0) velocity.x -= (decel * Time.deltaTime);
-        else if (velocity.x > 0) velocity.x += (decel * Time.deltaTime);
-        if (velocity.y < -0) velocity.y -= (decel * Time.deltaTime);
-        else if (velocity.y > 0) velocity.y += (decel * Time.deltaTime);
+        float speed = velocity.magnitude;
+        speed = Mathf.MoveTowards(speed, 0f, decel * Time.deltaTime);
+        if (speed > 0f) velocity = velocity.normalized * speed;
+        else velocity = Vector3.zero;
 
         //Set Player Position
         position.x += (velocity.x * Time.deltaTime);
         position.y += (velocity.y * Time.deltaTime);
         transform.position = position;
+
+        //Move Camera to Player Position
+        cam.transform.position = new Vector3(position.x, position.y, -10f);
+
+        //Rotate Sprite Towards Velocity Over Time If Moving
+        direction = velocity.normalized;
+        if (direction != Vector3.zero)
+        {
+            float targetAngle = (Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg) - 90f;
+            Quaternion targetRotation = Quaternion.Euler(0f, 0f, targetAngle);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotSpeed * Time.deltaTime);
+        }
     }
 
     //==== FUNCTIONS ====
