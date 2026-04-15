f0a2135f93ded92c7cbb2c971bf6fc649eb01ab5
searcherdev <searcherdev@gmail.com>
2026-04-15 15:15:42 -0400

    Cursor yay yippee

---
+++ Searcher/Assets/Scripts/ItemManager.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class ItemManager : MonoBehaviour
+{
+    //==== FIELDS ====
+
+    
+    //==== START ====
+    void Start()
+    {
+        
+    }
+
+    //==== UPDATE ====
+    void Update()
+    {
+        
+    }
+}
+
+/*
+ 
+WHAT'S THIS CLASS FOR?
+This class will contain information for every item in the game, from basic Resources to complex pieces of Equipment.
+ 
+ */
