e3e587e3d580fcfa9ebd87c327e365974a7a22e4
searcherdev <searcherdev@gmail.com>
2026-06-10 17:52:32 -0400

    Cursor multi-targeting fix

--- Searcher/Assets/Scripts/Cursor.cs
+++ Searcher/Assets/Scripts/Cursor.cs
@@ -41,7 +41,7 @@ public class Cursor : MonoBehaviour
         bool mouseColliding = false;
         foreach (GameObject a in manager.GetComponent<AsteroidManager>().Asteroids)
         {
-            if (collisions.CheckMouseOverlap(mousePos, a))
+            if (collisions.CheckMouseOverlap(mousePos, a) && target == null)
             {
                 cursor.transform.position = Vector3.MoveTowards(cursor.transform.position, a.transform.position, 50f * Time.deltaTime);
                 mouseColliding = true;
@@ -53,7 +53,7 @@ public class Cursor : MonoBehaviour
             cursor.transform.position = Vector3.MoveTowards(cursor.transform.position, mousePos, 50f * Time.deltaTime);
             foreach (GameObject n in manager.GetComponent<NebulaManager>().Nebulae) //Now that we know for sure the cursor isn't over an Asteroid, check if we're over a Nebula
             {
-                if (collisions.CheckMouseOverlap(mousePos, n))
+                if (collisions.CheckMouseOverlap(mousePos, n) && target == null)
                 {
                     target = n;
                 }
